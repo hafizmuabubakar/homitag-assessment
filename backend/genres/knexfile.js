a8145c943909714b4298827aba2e0f459efc74e3
Hafiz Abubakar <hafizmuabubakar@gmail.com>
2023-01-18 16:02:02 +0500

    Updated Dpocker File

--- backend/genres/knexfile.js
+++ backend/genres/knexfile.js
@@ -2,8 +2,10 @@ module.exports = {
 
 	development: {
 		client: 'pg',
-		connection: 'postgres://rfjwqhebnecszl:de7cd9e108da3a4d035317921fd2b6dd0cc4da2d1424c93f5ee8db012700a3ab@ec2-3-209-124-113.compute-1.amazonaws.com:5432/d7rpujhbg84p5p',
-		ssl: { rejectUnauthorized: false },
+		connection: {
+			connectionString: 'postgres://rfjwqhebnecszl:de7cd9e108da3a4d035317921fd2b6dd0cc4da2d1424c93f5ee8db012700a3ab@ec2-3-209-124-113.compute-1.amazonaws.com:5432/d7rpujhbg84p5p',
+			ssl: { rejectUnauthorized: false },
+		},
 		migrations: {
 			directory: './migrations',
 		},
